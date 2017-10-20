38819e2661d4fd47ac55c4100423e83488b0a852
Abhishek M R <abhishekmarv@gmail.com>
2017-10-20 08:08:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhi/bottomsheet/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.abhi.bottomsheet;
+
+import android.content.Intent;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomSheetBehavior mBottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View bottomSheet = findViewById( R.id.bottom_sheet );
+        Button button1 = (Button) findViewById( R.id.button_1 );
+        Button button2 = (Button) findViewById( R.id.button_2 );
+        Button button3 = (Button) findViewById( R.id.button_3 );
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, Carpool.class));
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, Carpool.class));
+            }
+        });
+
+
+
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setPeekHeight(200);
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+    }
+
+
+}
