c9322f4a0691a774259482b586d9d45c55585201
Abhishek M R <abhishekmarv@gmail.com>
2017-10-27 22:16:55 +0530

    service added

---
+++ app/src/main/java/com/example/abhi/bottomsheet/ServiceChat.java
@@ -0,0 +1,223 @@
+package com.example.abhi.bottomsheet;
+
+/**
+ * Created by anuj on 27/10/17.
+ */
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttSecurityException;
+
+
+/**
+ * Created by amogh on 26/10/17.
+ */
+
+public class ServiceChat extends Service {
+
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "MyPref" ;
+    public static final String VAL = "Key";
+
+
+    String host = "tcp://m11.cloudmqtt.com:16201";
+    // String clientId = "ExampleAndroidClient";
+    String topic = "topic/iot";
+
+    String username = "rcduaeoh";
+    String password = "hm3O7P_0KiXi";
+
+    MqttAndroidClient client;
+    IMqttToken token = null;
+    MqttConnectOptions options;
+
+    int val = 0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), host, clientId);
+
+        options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        val = sharedpreferences.getInt(VAL, 0);
+
+        try {
+            token = client.connect(options);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(getApplicationContext(),"Connection successful",Toast.LENGTH_SHORT).show();
+                    subscribtion();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(getApplicationContext(),"Connection failed",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                String msg = new String(message.getPayload());
+                if(!msg.contains("value")) {
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    if (msg.equals("rhint")) {
+                        if (val != 0) {
+                            Toast.makeText(getApplicationContext(), msg + "fuck this shit", Toast.LENGTH_SHORT).show();
+                        }
+                        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putInt(VAL, 0);
+                        editor.commit();
+                        val = 0;
+                        startNotification();
+                    }
+                }
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+
+        Toast.makeText(this, "ServiceChat Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "ServiceChat Destroyed", Toast.LENGTH_LONG).show();
+    }
+
+    private void subscribtion(){
+        try {
+            client.subscribe(topic,0);
+        } catch (MqttSecurityException e) {
+            e.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startNotification(){
+
+        // Set Notification Title
+        String strtitle = getString(R.string.notificationtitle);
+        // Set Notification Text
+        String strtext = getString(R.string.notificationtext);
+
+        // Open NotificationView Class on Notification Click
+        Intent intent = new Intent(this, Ride_act.class);
+        // Send data to NotificationView Class
+        intent.putExtra("title", strtitle);
+        intent.putExtra("text", strtext);
+        // Open NotificationView.java Activity
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent intent2 = new Intent(this, Carpool_act.class);
+        // Send data to NotificationView Class
+        intent2.putExtra("title", strtitle);
+        intent2.putExtra("text", strtext);
+        // Open NotificationView.java Activity
+        PendingIntent pIntent2 = PendingIntent.getActivity(this, 0, intent2,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent intent3 = new Intent(this, Chat_act.class);
+        // Send data to NotificationView Class
+        intent3.putExtra("title", strtitle);
+        intent3.putExtra("text", strtext);
+        // Open NotificationView.java Activity
+        PendingIntent pIntent3 = PendingIntent.getActivity(this, 0, intent3,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Create Notification using NotificationCompat.Builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                // Set Icon
+                .setSmallIcon(R.drawable.logo)
+                // Set Ticker Message
+                .setTicker(getString(R.string.notificationticker))
+                // Set Title
+                .setContentTitle("Bla blah wants to carpool.")
+                // Set Text
+                .setContentText(getString(R.string.notificationtext))
+                .addAction(R.drawable.cast_ic_notification_0, "Accept", pIntent)
+                // Add an Action Button below Notification
+                .addAction(R.drawable.cast_ic_notification_0, "Reject", pIntent2)
+
+                .addAction(R.drawable.cast_ic_notification_0, "Message", pIntent3)
+                // Set PendingIntent into Notification
+                .setContentIntent(pIntent)
+                // Dismiss Notification
+                .setAutoCancel(true);
+
+
+        // Create Notification Manager
+        NotificationManager notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Build Notification with Notification Manager
+        notificationmanager.notify(0, builder.build());
+
+
+    }
+
+}
